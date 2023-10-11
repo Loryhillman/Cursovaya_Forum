5dfe9ff6ab63a82976eb01d982badeaffbf67ee6
Loryhillman <roman.popov.a@mail.ru>
2023-10-11 05:35:31 +0300

    что-то очень странное

--- forum/testindex7hago.py
+++ forum/testindex7hago.py
@@ -1,36 +1,34 @@
 from waitress import serve
-import sys
-from pprint import pprint
-    
+
 def render_template(template_name='index.html', context={}):
 
     html_str=""
-    with open(template_name, "r") as f:
+    with open(template_name, 'r') as f:
         html_str=f.read()
         html_str=html_str.format(**context)
-    
     return html_str
 
-def mainpage(environ):
-    return render_template(template_name='P:\Projects_from_python\Kursovaya_Forum\Forum\index.html', context={})
+def home(environ):
+    return render_template(template_name='index.html', context={})
 
-def topic(environ):
-    return render_template(template_name='topic.html', context={})
+def contact_us(environ):
+    return render_template(template_name='contact.html', context={})
 
 def app(environ, start_response):
-    path = environ.get("PATH_INFO")
+    path= environ.get("PATH_INFO")
     if path == "/":
-        data = mainpage(environ)
+        data = home(environ)
+    elif path == "/contact":
+        data = contact_us(environ)
     else:
         data = render_template(template_name='404.html', context={"path":path})
     data = data.encode("utf-8")
-    
     start_response(
         f"200 OK", [
-            ("Content-type", "text/html")
+            ("Content-type", "text/html"),
+            ("Content-Lenght", str(len("abc")))
         ]
     )
-
     return iter([data])
-print(sys.path)
+
 serve(app)
