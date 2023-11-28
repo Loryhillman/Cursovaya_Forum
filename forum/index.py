af327b94c155ed096a01fa361079843e1dcd671d
Loryhillman <roman.popov.a@mail.ru>
2023-11-28 22:38:17 +0300

    Верстка + задание

--- forum/index.py
+++ forum/index.py
@@ -1,48 +1,38 @@
 from waitress import serve
-import sys
 
-
-def render_template(template_name='index.html', context={}):
-
-    html_str=""
-    with open(template_name, "r", encoding="UTF-8") as f:
-        html_str=f.read()
-        html_str=html_str.format(**context)
-    
-    return html_str
-
-def mainpage(environ):
-    return render_template(template_name='Forum/templates/index.html', context={})
-
-def topic(environ):
-    return render_template(template_name='Forum/templates/topic.html', context={})
+from templates_view.index_view import IndexView
+from templates_view.topic_view import TopicView
+from render_template import render_template
+from serve_file import serve_file
+import mimetypes
 
 adresses = {
-    "/" : mainpage,
-    "/topic" : topic,
+    "/" : IndexView,
+    "/topic" : TopicView,
 }
-
-def adress_parse(path, environ):
-    data = adresses[path]
-    return data(environ)
-
 
 
 
 def app(environ, start_response):
     path = environ.get("PATH_INFO")
 
+    if path.startswith("/src/"):
+        return serve_file(path, start_response)
 
     if path in adresses:
-        data = adress_parse(path, environ)
+        view_class = adresses[path]
+        view_instance = view_class()
+        data = view_instance.get(environ)
+        content_type = 'text/html; charset=UTF-8'
     else:
         data = render_template(template_name="Forum/templates/404.html", context={})
+        content_type = 'text/html; charset=UTF-8'
         
     data = data.encode("utf-8")
     
     start_response(
         f"200 OK", [
-            ("Content-type", "text/html; charset=utf-8")
+            ("Content-type", content_type)
 
         ]
     )
